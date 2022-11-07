663530c9fb49397637653ed8ca1105acf9f4dd2f
efimovgeorge <100308528+efimovgeorge@users.noreply.github.com>
2022-11-07 18:30:46 +0300

    feat: create html-builder 3
    feat: create html-builder 3

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const { stdout } = require('process');
+const { readdir, stat } = require('fs/promises');
+
+const folder = 'secret-folder';
+const folderPath = path.join(__dirname, folder);
+const options = { withFileTypes: true };
+const delimiter = ' - ';
+
+const convertToKb = (bytes, digitsAfter = 3) => `${(bytes / 1024).toFixed(digitsAfter)}kb`;
+
+async function logFileInfo(fullPath, outputDelimiter) {
+  try {
+    const fileStat = await stat(fullPath);
+    const fileInfo = path.basename(fullPath).split('.');
+    fileInfo.push(convertToKb(fileStat.size));
+    stdout.write(`${fileInfo.join(outputDelimiter)}\n`);
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+async function getDirFilesInfo(folder, options, outputDelimiter) {
+  try {
+    const files = await readdir(folder, options);
+    files
+      .filter((file) => file.isFile())
+      .forEach((file) => {
+        logFileInfo(path.join(folder, file.name), outputDelimiter);
+      });
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+getDirFilesInfo(folderPath, options, delimiter);
